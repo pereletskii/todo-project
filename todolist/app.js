8dbcab88c2660423ee60c69ad0794b4910e4ac64
Gleb Gorelov <qgorelov@gmail.com>
2024-12-13 18:38:38 +0300

    Routing architecture and registration router were added

--- todolist/app.js
+++ todolist/app.js
@@ -3,26 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
 
-const registrationController = require('./controllers/registrationController');
-
-function baseInfo(req, res) {
-    console.log(req.path, req.query);
-    console.log(req.body);
-}
-
 app.use(bodyParser.json());
-// app.use(baseInfo);
 
-app.route('/registration')
-    .post(registrationController);
-
-app.route('/')
-    .get((req, res) => {
-        baseInfo(req, res);
-    })
-    .post((req, res) => {
-        baseInfo(req, res);
-    })
+const registrationRouter = require('./routers/registrationRouter');
+app.use('/registration', registrationRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
