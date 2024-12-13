3ad9c99667cf17a55ec25464315531626c02f8c9
Gleb Gorelov <qgorelov@gmail.com>
2024-12-13 21:56:15 +0300

    User info validation separated

--- todolist/models/users.js
+++ todolist/models/users.js
@@ -33,14 +33,3 @@ module.exports = sequelize.define('users',
     updatedAt: false
   }
 );
-
-function validateUser(user) {
-  const schema = Joi.object({
-      user_name: Joi.string().min(3).max(100).required(),
-      email: Joi.string().min(5).max(255).required().email(),
-      password: Joi.string().min(8).max(100).required()
-  })
-  return schema.validate(user)
-}
-
-module.exports.validate = validateUser
