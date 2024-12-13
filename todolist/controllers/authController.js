df5d0454ffba4dca2fbb252159f7ab6eaacc2ac0
Gleb Gorelov <qgorelov@gmail.com>
2024-12-13 22:34:24 +0300

    JWT token generation added

---
+++ todolist/controllers/authController.js
@@ -0,0 +1,44 @@
+const Users = require('../models/users'); 
+const bcrypt = require('bcrypt');
+const generateAccessToken = require('../services/jwtService');
+
+const authController = async (req, res, next) => {
+    const body = req.body;
+
+    if (!body.email || !body.password) {
+        return res.status(400).json({
+            success: false,
+            message: 'Bad request'
+        });
+    }
+
+    let user = await Users.findOne({ where:{ email: body.email } })
+
+    await bcrypt.compare(body.password, user.password).then(function(result) {
+        if (!result) {
+            return res.status(400).json({
+                success: false,
+                message: 'Wrong password'
+            });
+        } else {
+            let token = generateAccessToken({
+                id: user.id,
+                user_name: user.user_name,
+                email: user.email
+            });
+
+            return res.status(202)
+                .header('auth-token', token)
+                .json({
+                    success: true,
+                    message: {
+                        id: user.id,
+                        user_name: user.user_name,
+                        email: user.email
+                    }
+                });
+        }
+    });    
+}
+
+module.exports = authController;
---
+++ todolist/routers/allAuthRouter.js
@@ -0,0 +1,46 @@
+const Users = require('../models/users')
+const router = require('express').Router()
+const registrationController = require('../controllers/registrationController')
+const authController = require('../controllers/authController')
+const validateRegister = require('../services/validation').validateRegister
+const validateLogin = require('../services/validation').validateLogin
+
+router.post('/registration', async (req, res) => {
+    console.log(req.body)
+    const { error } = validateRegister(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    let user = await Users.findOne({ where:{ email: req.body.email } })
+    if (user) {
+        return res.status(400).send('User already exisits. Please sign in')
+    } else {
+        try {
+            await registrationController(req, res);
+        } catch (err) {
+            return res.status(400).json({ message: err.message })
+        }
+    }
+})
+
+router.post('/login', async (req, res) => {
+    console.log(req.body)
+    const { error } = validateLogin(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    let user = await Users.findOne({ where:{ email: req.body.email } })
+    if(!user) {
+        return res.status(400).send('User not found')
+    } else {
+        try {
+            await authController(req, res);
+        } catch (err) {
+            return res.status(400).json({ message: err.message })
+        }
+    }
+})
+
+module.exports = router
