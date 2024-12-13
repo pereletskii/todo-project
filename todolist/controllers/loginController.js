fcf5cc83e178573a78f9cd8f9e82a6ec88dfa0a3
Gleb Gorelov <qgorelov@gmail.com>
2024-12-13 17:05:36 +0300

    User registration controller created

---
+++ todolist/controllers/loginController.js
@@ -0,0 +1,30 @@
+const User = require('../models/user'); 
+
+const registrationController = (req, res, next) => {
+    const body = req.body;
+
+    if (!body.user_name || !body.email || !body.password) {
+        return res.status(400).json({
+            success: false,
+            message: 'Bad request'
+        });
+    }
+
+    const newUser = User.create({
+        user_name: body.user_name,
+        email: body.email,
+        password: body.password
+    })
+
+    if (!newUser) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        });
+    }
+
+    return res.status(201).json({
+        success: true,
+        message: newUser
+    });
+}
