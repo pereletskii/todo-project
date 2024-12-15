d3386b75df956e2b8182f355f5720732c8bbdb0c
Gleb Gorelov <qgorelov@gmail.com>
2024-12-15 13:36:54 +0300

    Expiration time for JWT tokens is now 1 day

--- todolist/services/jwtService.js
+++ todolist/services/jwtService.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const access_token = require('../env_config.json').jwt_access_token
 
 const generateAccessToken = (user_info) => {
-    return jwt.sign(user_info, access_token);
+    return jwt.sign(user_info, access_token, { expiresIn: '1d' });
 }
 
 module.exports = generateAccessToken
