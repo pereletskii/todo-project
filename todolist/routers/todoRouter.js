a49aa1a7c7821a3770da4ca6a0d62998e36a9caf
Gleb Gorelov <qgorelov@gmail.com>
2024-12-15 14:15:17 +0300

    Endpoints for lists get and create added

--- todolist/routers/todoRouter.js
+++ todolist/routers/todoRouter.js
@@ -1,20 +1,45 @@
 const Lists = require('../models/lists')
 const Tasks = require('../models/tasks')
 const authToken = require('../middlewares/authToken')
-
 const router = require('express').Router()
-/*
-* Controllers 
-*/
-/*
-* Validators
-*/
+
+const TodoController = require('../controllers/todoController')
+
+const validateLists = require('../services/validationService').validateLists
 
 router.use(authToken)
 
 router.get('/', async (req, res) => {
-    res.status(200).json(req.user_info);
-    // TODO: Add list JSON response for user
+    let lists = await TodoController.getLists(req)
+    
+    if (lists.length == 0) {
+        return res.status(204).json({
+            success: true,
+            message: 'No lists found'
+        })
+    } else {
+        return res.status(200).json({
+            success: true,
+            lists
+        })
+    }
+})
+
+router.post('/create-list', async (req, res) => {
+    const { error } = validateLists(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    } else {
+        try {
+            const list = await TodoController.createList(req)
+            return res.status(201).json({
+                success: true,
+                message: list
+            })
+        } catch (err) {
+            return res.status(400).json({ message: err.message })
+        }
+    }
 })
 
 module.exports = router
