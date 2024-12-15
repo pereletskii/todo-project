0f1319492a2bc427f3d5f3b4a01d52c51b4cccde
Gleb Gorelov <qgorelov@gmail.com>
2024-12-15 18:20:02 +0300

    Unused require statements removed

--- todolist/routers/todoRouter.js
+++ todolist/routers/todoRouter.js
@@ -1,5 +1,3 @@
-const Lists = require('../models/lists')
-const Tasks = require('../models/tasks')
 const authToken = require('../middlewares/authToken')
 const router = require('express').Router()
 
