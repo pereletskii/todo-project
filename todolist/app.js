2b54af8babd9cb7a3af3dad6b8cbcdace67edf1e
Gleb Gorelov <qgorelov@gmail.com>
2024-12-19 00:39:04 +0300

    Swagger init

--- todolist/app.js
+++ todolist/app.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const port = require('./env_config.json').port;
 const cookieParser = require('cookie-parser')
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(bodyParser.json());
 app.use(cookieParser())
 
